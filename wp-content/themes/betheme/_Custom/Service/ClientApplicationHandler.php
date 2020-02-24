c258915e45a375b2653341d4f9ede38eb0b26fe4
Nkmelnikov <Nkmelnikov@hotmail.com>
2020-02-24 14:09:03 +0300

    count age / and others methods.

--- wp-content/themes/betheme/_Custom/Service/ClientApplicationHandler.php
+++ wp-content/themes/betheme/_Custom/Service/ClientApplicationHandler.php
@@ -119,4 +119,63 @@ class ClientApplicationHandler
         return false;
     }
 
+    public function convertImgPath($src){
+        preg_match_all('`(\/wp-content.*)`im', $src, $new_src, PREG_SET_ORDER);
+        if (!empty($new_src)) {
+            return $new_src[0][0];
+        }
+        return '';
+    }
+
+    public function countAge($dateOfBirth){
+        $date = new DateTime($dateOfBirth);
+        $now = new DateTime();
+        $interval = $now->diff($date);
+        return $interval->y;
+    }
+
+    public function getZodiac($dateOfBirth){
+        $d = explode('-', $dateOfBirth);
+        $day = $d[2];
+        $month = $d[1];
+
+        if(($month==1 && $day>20)||($month==2 && $day<20)) {
+            $mysign = "aquarius";
+        }
+        if(($month==2 && $day>18 )||($month==3 && $day<21)) {
+            $mysign = "pisces";
+        }
+        if(($month==3 && $day>20)||($month==4 && $day<21)) {
+            $mysign = "aries";
+        }
+        if(($month==4 && $day>20)||($month==5 && $day<22)) {
+            $mysign = "taurus";
+        }
+        if(($month==5 && $day>21)||($month==6 && $day<22)) {
+            $mysign = "gemini";
+        }
+        if(($month==6 && $day>21)||($month==7 && $day<24)) {
+            $mysign = "cancer";
+        }
+        if(($month==7 && $day>23)||($month==8 && $day<24)) {
+            $mysign = "leo";
+        }
+        if(($month==8 && $day>23)||($month==9 && $day<24)) {
+            $mysign = "virgo";
+        }
+        if(($month==9 && $day>23)||($month==10 && $day<24)) {
+            $mysign = "libra";
+        }
+        if(($month==10 && $day>23)||($month==11 && $day<23)) {
+            $mysign = "scorpio";
+        }
+        if(($month==11 && $day>22)||($month==12 && $day<23)) {
+            $mysign = "sagittarius";
+        }
+        if(($month==12 && $day>22)||($month==1 && $day<21)) {
+            $mysign = "capricorn";
+        }
+        return $mysign;
+    }
+
 }
