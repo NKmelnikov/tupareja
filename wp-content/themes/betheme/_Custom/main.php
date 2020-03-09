948c3b8d4c4a00a33b14b1cb9f5b008c6241f627
SkorpmaX <max.skorpmax@yandex.by>
2020-03-10 00:55:41 +0300

    fix deepSearch v2

--- wp-content/themes/betheme/_Custom/main.php
+++ wp-content/themes/betheme/_Custom/main.php
@@ -33,61 +33,106 @@ $pathToCustom = '/wp-content/themes/betheme/_Custom/';
 <form id="mp1-ds-form" action="">
 	<section class="mp1-ds-age-section">
 		<span>Возраст</span>
-		<label for="mp1-ds-age-from">От</label>
-		<input id="mp1-ds-age-from" name="mp1-ds-age-from" value="18" type="text">
-		<label for="mp1-ds-age-to">До</label>
-		<input for="mp1-ds-age-to" name="mp1-ds-age-to" value="60" type="text">
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-age-from">От</label>
+			<input id="mp1-ds-age-from" name="mp1-ds-age-from" value="18" type="text">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-age-to">До</label>
+			<input for="mp1-ds-age-to" name="mp1-ds-age-to" value="60" type="text">
+		</div>
 	</section>
 	<section class="mp1-ds-height-section">
 		<span>Рост</span>
-		<label for="mp1-ds-height-from">От</label>
-		<input id="mp1-ds-height-from" name="mp1-ds-height-from" value="160" type="text">
-		<label for="mp1-ds-height-to">До</label>
-		<input for="mp1-ds-height-to" name="mp1-ds-height-to" value="190" type="text">
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-height-from">От</label>
+			<input id="mp1-ds-height-from" name="mp1-ds-height-from" value="160" type="text">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-height-to">До</label>
+			<input for="mp1-ds-height-to" name="mp1-ds-height-to" value="190" type="text">
+		</div>
 	</section>
 	<section class="mp1-ds-weight-section">
 		<span>Вес</span>
-		<label for="mp1-ds-weight-from">От</label>
-		<input id="mp1-ds-weight-from" name="mp1-ds-weight-from" value="45" type="text">
-		<label for="mp1-ds-weight-to">До</label>
-		<input for="mp1-ds-weight-to" name="mp1-ds-weight-to" value="70" type="text">
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-weight-from">От</label>
+			<input id="mp1-ds-weight-from" name="mp1-ds-weight-from" value="45" type="text">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-weight-to">До</label>
+			<input for="mp1-ds-weight-to" name="mp1-ds-weight-to" value="70" type="text">
+		</div>
 	</section>
 	<section class="mp1-ds-zodiac-section">
-		<label for="mp1-ds-aquarius">Водолей</label>
-		<input id="mp1-ds-aquarius" class="zodiac-input" name="mp1-ds-aquarius" type="checkbox">
-		<label for="mp1-ds-pisces">Рыбы</label>
-		<input id="mp1-ds-pisces" class="zodiac-input" name="mp1-ds-pisces" type="checkbox">
-		<label for="mp1-ds-aries">Овен</label>
-		<input id="mp1-ds-aries" class="zodiac-input" name="mp1-ds-aries" type="checkbox">
-		<label for="mp1-ds-taurus">Телец</label>
-		<input id="mp1-ds-taurus" class="zodiac-input" name="mp1-ds-taurus" type="checkbox">
-		<label for="mp1-ds-gemini">Близнецы</label>
-		<input id="mp1-ds-gemini" class="zodiac-input" name="mp1-ds-gemini" type="checkbox">
-		<label for="mp1-ds-cancer">Рак</label>
-		<input id="mp1-ds-cancer" class="zodiac-input" name="mp1-ds-cancer" type="checkbox">
-		<label for="mp1-ds-leo">Лев</label>
-		<input id="mp1-ds-leo" class="zodiac-input" name="mp1-ds-leo" type="checkbox">
-		<label for="mp1-ds-virgo">Дева</label>
-		<input id="mp1-ds-virgo" class="zodiac-input" name="mp1-ds-virgo" type="checkbox">
-		<label for="mp1-ds-libra">Весы</label>
-		<input id="mp1-ds-libra" class="zodiac-input" name="mp1-ds-libra" type="checkbox">
-		<label for="mp1-ds-scorpio">Скорпион</label>
-		<input id="mp1-ds-scorpio" class="zodiac-input" name="mp1-ds-scorpio" type="checkbox">
-		<label for="mp1-ds-sagittarius">Стрелец</label>
-		<input id="mp1-ds-sagittarius" class="zodiac-input" name="mp1-ds-sagittarius" type="checkbox">
-		<label for="mp1-ds-capricorn">Козерог</label>
-		<input id="mp1-ds-capricorn" class="zodiac-input" name="mp1-ds-capricorn" type="checkbox">
+		<span>Знак зодиака</span>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-aquarius">Водолей</label>
+			<input id="mp1-ds-aquarius" class="zodiac-input" name="mp1-ds-aquarius" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-pisces">Рыбы</label>
+			<input id="mp1-ds-pisces" class="zodiac-input" name="mp1-ds-pisces" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-aries">Овен</label>
+			<input id="mp1-ds-aries" class="zodiac-input" name="mp1-ds-aries" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-taurus">Телец</label>
+			<input id="mp1-ds-taurus" class="zodiac-input" name="mp1-ds-taurus" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-gemini">Близнецы</label>
+			<input id="mp1-ds-gemini" class="zodiac-input" name="mp1-ds-gemini" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-cancer">Рак</label>
+			<input id="mp1-ds-cancer" class="zodiac-input" name="mp1-ds-cancer" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-leo">Лев</label>
+			<input id="mp1-ds-leo" class="zodiac-input" name="mp1-ds-leo" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-virgo">Дева</label>
+			<input id="mp1-ds-virgo" class="zodiac-input" name="mp1-ds-virgo" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-libra">Весы</label>
+			<input id="mp1-ds-libra" class="zodiac-input" name="mp1-ds-libra" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-scorpio">Скорпион</label>
+			<input id="mp1-ds-scorpio" class="zodiac-input" name="mp1-ds-scorpio" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-sagittarius">Стрелец</label>
+			<input id="mp1-ds-sagittarius" class="zodiac-input" name="mp1-ds-sagittarius" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-capricorn">Козерог</label>
+			<input id="mp1-ds-capricorn" class="zodiac-input" name="mp1-ds-capricorn" type="checkbox">
+		</div>
 	</section>
 	<section class="mp1-ds-eye-section">
 		<span>Цвет глаз</span>
-		<label for="mp1-ds-eyes-blue">Голубые</label>
-		<input id="mp1-ds-eyes-blue" class="eyes-input" name="mp1-ds-eyes-blue" type="checkbox">
-		<label for="mp1-ds-eyes-hazel">Карьи</label>
-		<input id="mp1-ds-eyes-hazel" class="eyes-input" name="mp1-ds-eyes-hazel" type="checkbox">
-		<label for="mp1-ds-eyes-gray">Серые</label>
-		<input id="mp1-ds-eyes-gray" class="eyes-input" name="mp1-ds-eyes-gray" type="checkbox">
-		<label for="mp1-ds-eyes-green">Зелёные</label>
-		<input id="mp1-ds-eyes-green" class="eyes-input" name="mp1-ds-eyes-green" type="checkbox">
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-eyes-blue">Голубые</label>
+			<input id="mp1-ds-eyes-blue" class="eyes-input" name="mp1-ds-eyes-blue" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-eyes-hazel">Карьи</label>
+			<input id="mp1-ds-eyes-hazel" class="eyes-input" name="mp1-ds-eyes-hazel" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-eyes-gray">Серые</label>
+			<input id="mp1-ds-eyes-gray" class="eyes-input" name="mp1-ds-eyes-gray" type="checkbox">
+		</div>
+		<div class="mp1-ds-form-line">
+			<label for="mp1-ds-eyes-green">Зелёные</label>
+			<input id="mp1-ds-eyes-green" class="eyes-input" name="mp1-ds-eyes-green" type="checkbox">
+		</div>
 	</section>
 	<button id="mp1-ds-submit-btn" type="submit">Искать</button>
 </form>
