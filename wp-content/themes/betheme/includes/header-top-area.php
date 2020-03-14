d2787675bbe71bafd42cf03526c6018d82f2e2da
Nkmelnikov <Nkmelnikov@hotmail.com>
2020-03-14 15:48:08 +0300

    header not in function

--- wp-content/themes/betheme/includes/header-top-area.php
+++ wp-content/themes/betheme/includes/header-top-area.php
@@ -10,47 +10,47 @@ $action_bar = mfn_opts_get('action-bar');
 $translate['wpml-no'] = mfn_opts_get('translate') ? mfn_opts_get('translate-wpml-no', 'No translations available for this page') : __('No translations available for this page', 'betheme');
 ?>
 
-<?php if ( ('1' === $action_bar) || isset($action_bar['show']) ): ?>
-	<div id="Action_bar">
-		<div class="container">
-			<div class="column one">
+<?php if (('1' === $action_bar) || isset($action_bar['show'])): ?>
+  <div id="Action_bar">
+    <div class="container">
+      <div class="column one">
 
-				<?php
-					get_template_part('includes/include', 'slogan');
+          <?php
+          get_template_part('includes/include', 'slogan');
 
-					if (has_nav_menu('social-menu')) {
-						mfn_wp_social_menu();
-					} else {
-						get_template_part('includes/include', 'social');
-					}
-				?>
+          if (has_nav_menu('social-menu')) {
+              mfn_wp_social_menu();
+          } else {
+              get_template_part('includes/include', 'social');
+          }
+          ?>
 
-			</div>
-		</div>
-	</div>
+      </div>
+    </div>
+  </div>
 <?php endif; ?>
 
 <?php
-	if (mfn_header_style(true) == 'header-overlay') {
+if (mfn_header_style(true) == 'header-overlay') {
 
-		// overlay menu
+    // overlay menu
 
-		echo '<div id="Overlay">';
-			mfn_wp_overlay_menu();
-		echo '</div>';
+    echo '<div id="Overlay">';
+    mfn_wp_overlay_menu();
+    echo '</div>';
 
-		// button
+    // button
 
-		echo '<a class="overlay-menu-toggle" href="#">';
-			echo '<i class="open icon-menu-fine"></i>';
-			echo '<i class="close icon-cancel-fine"></i>';
-		echo '</a>';
-	}
+    echo '<a class="overlay-menu-toggle" href="#">';
+    echo '<i class="open icon-menu-fine"></i>';
+    echo '<i class="close icon-cancel-fine"></i>';
+    echo '</a>';
+}
 ?>
 
 <div id="Top_bar">
-	<div class="container">
-		<div class="column one">
+  <div class="container">
+    <div class="column one">
       <div class="heart-box heart-box-main-header">
         <img class="heart-box__heart-small rotated header-section hs1" src="/wp-content/themes/betheme/_Custom/_static/img/small-left.png" alt="">
         <img class="heart-box__heart-medium header-section hs2" src="/wp-content/themes/betheme/_Custom/_static/img/medium-right.png" alt="">
@@ -60,57 +60,83 @@ $translate['wpml-no'] = mfn_opts_get('translate') ? mfn_opts_get('translate-wpml
           <?php get_template_part('includes/include', 'logo'); ?>
         <p class="header-special-mobile__title">International Matchmaking Office</p>
         <a class="responsive-menu-toggle" href="#"><i class="icon-menu-fine"></i><a>
-            <?php mfn_wp_nav_menu(); ?>
+                <?php mfn_wp_nav_menu(); ?>
       </div>
       <script>
           $('.responsive-menu-toggle').click(function () {
-            $('#menu').toggleClass('active');
+              $('#menu').toggleClass('active');
           })
       </script>
 
 
-			<div class="top_bar_left clearfix">
+      <div class="top_bar_left clearfix">
 
-				<?php get_template_part('includes/include', 'logo'); ?>
-				<div class="header-right-block">
-					<div class="header-title">
-						<h1>International Matchmaking Office</h1>
-					</div>
-					<div class="menu_wrapper">
-						<?php
-							if ((mfn_header_style(true) != 'header-overlay') && (mfn_opts_get('menu-style') != 'hide')) {
+          <?php get_template_part('includes/include', 'logo'); ?>
+        <div class="header-right-block">
+          <div class="header-title">
+            <h1>International Matchmaking Office</h1>
+          </div>
+          <div class="menu_wrapper">
+              <?php
+              if ((mfn_header_style(true) != 'header-overlay') && (mfn_opts_get('menu-style') != 'hide')) {
 
-								// main menu
+                  // main menu
 
-								mfn_wp_nav_menu();
+                  ?>
+                <nav id="menu">
+                  <ul id="menu-menu-1" class="menu menu-main">
+                    <li id="menu-item-87" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-82 current_page_item">
+                      <a href="http://melnikovshop.loc/"><span>Главная</span></a>
+                    </li>
+                    <li id="menu-item-86" class="menu-item menu-item-type-post_type menu-item-object-page">
+                      <a href="http://melnikovshop.loc/our-ladies/"><span>Галерея девушек</span></a>
+                    </li>
+                    <li id="menu-item-107" class="menu-item menu-item-type-post_type menu-item-object-page">
+                      <a href="http://melnikovshop.loc/service/"><span>Услуги</span></a>
+                    </li>
+                    <li id="menu-item-138" class="menu-item menu-item-type-post_type menu-item-object-page">
+                      <a href="http://melnikovshop.loc/tips-tricks/"><span>Советы и рекомендации</span></a>
+                    </li>
+                    <li id="menu-item-139" class="menu-item menu-item-type-post_type menu-item-object-page">
+                      <a href="http://melnikovshop.loc/for-ladies/"><span>Для девушек</span></a>
+                    </li>
+                    <li id="menu-item-140" class="menu-item menu-item-type-post_type menu-item-object-page">
+                      <a href="http://melnikovshop.loc/men-application/"><span>Заявка для мужчин</span></a>
+                    </li>
+                    <li id="menu-item-141" class="menu-item menu-item-type-post_type menu-item-object-page">
+                      <a href="http://melnikovshop.loc/contact/"><span>Контакты</span></a>
+                    </li>
+                  </ul>
+                </nav>
+                  <?php
 
-								// responsive menu button
+                  // responsive menu button
 
-								$mb_class = '';
-								if (mfn_opts_get('header-menu-mobile-sticky')) {
-									$mb_class .= ' is-sticky';
-								}
+                  $mb_class = '';
+                  if (mfn_opts_get('header-menu-mobile-sticky')) {
+                      $mb_class .= ' is-sticky';
+                  }
 
-								echo '<a class="responsive-menu-toggle '. esc_attr($mb_class) .'" href="#">';
-								if ($menu_text = trim(mfn_opts_get('header-menu-text'))) {
-									echo '<span>'. wp_kses($menu_text, mfn_allowed_html()) .'</span>';
-								} else {
-									echo '<i class="icon-menu-fine"></i>';
-								}
-								echo '</a>';
-							}
-						?>
-					</div>
-				</div>
+                  echo '<a class="responsive-menu-toggle ' . esc_attr($mb_class) . '" href="#">';
+                  if ($menu_text = trim(mfn_opts_get('header-menu-text'))) {
+                      echo '<span>' . wp_kses($menu_text, mfn_allowed_html()) . '</span>';
+                  } else {
+                      echo '<i class="icon-menu-fine"></i>';
+                  }
+                  echo '</a>';
+              }
+              ?>
+          </div>
+        </div>
 
-			</div>
+      </div>
 
-			<?php
-				if (! mfn_opts_get('top-bar-right-hide')) {
-					get_template_part('includes/header', 'top-bar-right');
-				}
-			?>
+        <?php
+        if (!mfn_opts_get('top-bar-right-hide')) {
+            get_template_part('includes/header', 'top-bar-right');
+        }
+        ?>
 
-		</div>
-	</div>
+    </div>
+  </div>
 </div>
