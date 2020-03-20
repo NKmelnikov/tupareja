5a445c1525e1e6c653eef2021dbc72786f1557cc
SkorpmaX <max.skorpmax@yandex.by>
2020-03-20 23:53:50 +0300

    About 1.2 with translation

--- wp-content/themes/betheme/_Custom/about.php
+++ wp-content/themes/betheme/_Custom/about.php
@@ -18,7 +18,7 @@ $pathToCustom = '/wp-content/themes/betheme/_Custom/';
 
 <section class="about-wrapper-title">
 	<div class="about-title">
-		<h2><?php _e('about', 'betheme') ?></h2>
+		<h2><?php _e('about_title', 'betheme') ?></h2>
 	</div>
 </section>
 <section class="about">
@@ -27,7 +27,7 @@ $pathToCustom = '/wp-content/themes/betheme/_Custom/';
 			<img src="<?= $pathToCustom . '_static/img/about_img_1.png'?>">
 		</div>
 		<div class="right">
-			<p>Меня зовут Лена, я родилась в Украине, городе-герое Киеве.<br/>Хочу поделиться с вами своим собственным опытом.<br/>Моя история началась в далеком 2007 году, когда разочаровавшись в поисках своего мужчины в Украине, я решила попытать свое счастье на чужбине, среди иностранных мужчин, и мой выбор упал на теплую Испанию, поскольку у меня было преимущество, и я знала испанский язык, факультет испанской филологии мне в этом помог, я начала свои поиски.</p>
+			<p><?php _e('about_elem_1', 'betheme') ?></p>
 		</div>
 	</div>
 	<div class="about_line line_2">
@@ -35,7 +35,7 @@ $pathToCustom = '/wp-content/themes/betheme/_Custom/';
 			<img src="<?= $pathToCustom . '_static/img/about_img_2.png'?>">
 		</div>
 		<div class="left">
-			<p>Если быть откровенной, я три года переписывалась с мужчинами в разных мессенджерах, но так и не получалось найти своего единственного, сильного и надежного. Но внутри себя, я все равно знала, что рано или поздно я все равно найду его. Конечно, если бы тогда мне смогли помочь в моих долгих поисках в огромной сети интернет, я бы не потеряла свое драгоценное время на пустую переписку несерьезных кандидатов. Ты же не знаешь, кто на самом деле находится по ту сторону экрана, его реальные намерения, цели и т.д. Но в одно прекрасное утро, я получила сообщение из Барселоны от моего будущего мужа (тогда я была еще далека от этой мысли) и что самое удивительное, он приехал знакомиться со мной в Украину, через две недели нашей переписки.</p>
+			<p><?php _e('about_elem_2', 'betheme') ?></p>
 		</div>
 	</div>
 
@@ -44,7 +44,7 @@ $pathToCustom = '/wp-content/themes/betheme/_Custom/';
 			<img src="<?= $pathToCustom . '_static/img/about_img_3.png'?>">
 		</div>
 		<div class="right">
-			<p>Мы много гуляли по городу Киеву, много разговаривали, наши цели и намерения были одинаковые. Мы оба хотели создать семейное уютное гнездышко, построить доверительные крепкие отношения, ведь для каждого из нас это главная цель. Меня абсолютно не пугал менталитет другой страны, мне это стало даже интересным, создать межнациональную семью, где могу внести особенности нашей украинской богатой культуры, а также разделять другие культурные особенности теплой Испании. Через два года наших отношений мы решили пожениться, этот день был самым счастливым в нашей жизни.</p>
+			<p><?php _e('about_elem_3', 'betheme') ?></p>
 		</div>
 	</div>
 	<div class="about_line line_2">
@@ -52,25 +52,21 @@ $pathToCustom = '/wp-content/themes/betheme/_Custom/';
 			<img src="<?= $pathToCustom . '_static/img/about_img_4.png'?>">
 		</div>
 		<div class="left">
-			<p>Сейчас мы воспитываем двоих деток, я должны была выучить каталонский язык и интегрироваться в новое для меня общество и окружение, но какая у меня мотивация – это моя любовь, во имя любви мы способны на многое.
-
-				Кто он испанский мужчина, кто она украинская девушка, какое сходство и отличие, давайте разберемся подробнее (ссылка)
-
-				Дорогие девушки, наша основная цель – предоставить вам уникальную возможность познакомиться с испанскими надежными мужчинам, а испанским мужчинам найти свою музу среди украинских красавиц, которые ищут именно вас и чтобы вы смогли построить добрые, крепкие, стабильные и доверительные отношения, а все остальное зависит от вас.</p>
+			<p><?php _e('about_elem_4', 'betheme') ?></p>
 		</div>
 	</div>
 </section>
 <section class="guarant">
 	<div class="guarant_border">
 		<div class="guarant_title_wrap">
-			<h2>Service Guarantee</h2>
-			<p>Best service</p>
+			<h2><?php _e('guarant_title', 'betheme') ?></h2>
+			<p><?php _e('guarant_title_text', 'betheme') ?></p>
 		</div>
 		<div class="guarant_elem_wrap">
-			<div class="guarant_elem"><p class="top">5</p><p class="bottom">Лет на рынке</p></div>
-			<div class="guarant_elem"><p class="top">75</p><p class="bottom">Соеденено сердец</p></div>
-			<div class="guarant_elem"><p class="top">375</p><p class="bottom">Невест на сайте</p></div>
-			<div class="guarant_elem"><p class="top">523</p><p class="bottom">Мужчины в поиске</p></div>
+			<div class="guarant_elem"><p class="top">5</p><p class="bottom"><?php _e('guarant_elem_1', 'betheme') ?></p></div>
+			<div class="guarant_elem"><p class="top">75</p><p class="bottom"><?php _e('guarant_elem_2', 'betheme') ?></p></div>
+			<div class="guarant_elem"><p class="top">375</p><p class="bottom"><?php _e('guarant_elem_3', 'betheme') ?></p></div>
+			<div class="guarant_elem"><p class="top">523</p><p class="bottom"><?php _e('guarant_elem_4', 'betheme') ?></p></div>
 		</div>
 	</div>
 </section>
